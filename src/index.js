8a92ad0c95c1063b5278bea2f0f6d39a8efee761
Sourabh thapliyal <75442117+Sourabh745@users.noreply.github.com>
2024-03-17 13:27:39 +0530

    change application icon

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
     <App />
   </React.StrictMode>
 );
