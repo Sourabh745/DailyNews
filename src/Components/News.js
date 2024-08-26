0c453340035d7cbfd7f0d2f9fb496c670a737cba
Sourabh thapliyal <75442117+Sourabh745@users.noreply.github.com>
2024-08-26 16:13:21 +0530

    Update News.js

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,112 +1,3 @@
-// import React, {useState, useEffect} from 'react';
-// import PropTypes from 'prop-types';
-// import NewsItem from './NewsItem';
-// import Spinner from './Spinner';
-// import InfiniteScroll from "react-infinite-scroll-component";
-
-
-// const News=(props) => {
-
-//   const [articles, setArticles] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [page, setPage] = useState(1);
-//   const [totalResults, setTotalResults] = useState(0);
-  
-// // document.title = `${capitalLetter(props.category)} - NewsMonkey`;
-
-//     const capitalLetter =(string) => {
-//         return string.charAt(0).toUpperCase() + string.slice(1);
-//       }
-
-//   const updateNews = async() => {
-//     props.setProgress(10);
-//     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=bd0bf1ce93004c95a41fd34fda9d823c&page=${props.page}&pageSize=${props.pageSize}`;
-//     setLoading(true);
-//     let data = await fetch(url);
-//     props.setProgress(30);
-//     let parseData = await data.json();
-//     props.setProgress(70);
-//     //console.log(parseData);
-//       setArticles(parseData.articles)
-//       setTotalResults(parseData.totalResults)
-//       setLoading(false)
-//     props.setProgress(100);
-//   }
-
-//   useEffect(() => {
-//     setPage = page+1;
-//     updateNews();
-//     // eslint-disable-next-line
-//   }, [page+1]);
-
-//   const fetchMoreData = async() =>{
-//     setPage(page+1)
-//     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=bd0bf1ce93004c95a41fd34fda9d823c&page=${page}&pageSize=${props.pageSize}`;
-//     let data = await fetch(url);
-//     let parseData = await data.json();
-//     console.log(parseData);   
-
-//     //console.log(parseData);
-    
-//       setArticles(articles.concat(parseData.articles))
-//       setTotalResults(parseData.totalResults)
-//       setLoading(false)
-//   }
-
-
-//   // const handlePrev =async() => {
-//   //   setPage(page - 1 );
-//   //   updateNews();
-//   // }
-
-//   // const handleNext = async() => {
-//   //   setPage(page + 1 );
-//   //   updateNews();
-//   // }
-
-//     return (
-//       <>
-//         <h1 className="text-center" style={{marginTop: "5%" }}>NewsMonkey Top headlines of {capitalLetter(props.category)}</h1>
-//         {loading && <Spinner/>}
-//         <InfiniteScroll
-//           dataLength={articles.length}
-//           next={fetchMoreData}
-//           hasMore={articles.length !== totalResults}
-//           loader={<Spinner/>}
-//         >
-//           <div className="container">
-//           <div className="row text-center">
-//             {/*!state.loading &&*/ articles.map((element) => {
-//               return <div className="col-md-4" key={element.url}>
-//                 <NewsItem title={element.title/*?element.title.slice(0,45):""*/} date={element.publishedAt} descriptions={element.description ? element.description.slice(0, 88) : ""} urlImage={element.urlToImage} newsUrl={element.url} author={element.author} source={element.source.name} />
-//               </div>
-//             })
-//             }
-//           </div>
-//           </div>
-          
-//         </InfiniteScroll>
-//         {/* <div className="container d-flex justify-content-between my-3">
-//           <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrev}>&larr; Previous</button>
-//           <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
-//         </div> */}
-
-//       </>
-//     );
-// }
-
-// News.defaultProps = {
-//     pageSize: 5,
-//     country: 'us',
-//     category: "general",
-//   }
-// News.propTypes = {
-//     pageSize : PropTypes.number,
-//     country : PropTypes.string,
-//     category : PropTypes.string ,
-// }
-// export default News;
-
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import NewsItem from './NewsItem';
@@ -118,37 +9,34 @@ const News = (props) => {
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
-  const [hasMore, setHasMore] = useState(true);
 
   const capitalLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  const updateNews = async (pageNumber) => {
+  const updateNews = async (newPage = 1) => {
     try {
       props.setProgress(10);
-      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=1d3f3019476e4d0aafdcba35615b8526&page=${pageNumber}&pageSize=${props.pageSize}`;
+      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=bd0bf1ce93004c95a41fd34fda9d823c&page=${newPage}&pageSize=${props.pageSize}`;
       setLoading(true);
       let response = await fetch(url);
-      
+
       if (response.status === 429) {
-        console.error('Rate limit exceeded. Please try again later.');
+        // Handle rate limit exceeded
+        console.error('Rate limit exceeded. Try again later.');
         setLoading(false);
-        setHasMore(false);
         return;
       }
 
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
-      
+
       props.setProgress(30);
       let parseData = await response.json();
       props.setProgress(70);
-
-      setArticles(prevArticles => pageNumber === 1 ? parseData.articles : [...prevArticles, ...parseData.articles]);
+      setArticles((prevArticles) => newPage === 1 ? parseData.articles : [...prevArticles, ...parseData.articles]);
       setTotalResults(parseData.totalResults);
-      setHasMore(parseData.articles.length > 0);
       setLoading(false);
       props.setProgress(100);
     } catch (error) {
@@ -163,9 +51,8 @@ const News = (props) => {
   }, [page]);
 
   const fetchMoreData = () => {
-    if (!loading && hasMore) {
-      setPage(prevPage => prevPage + 1);
-    }
+    if (loading) return; // Avoid making requests while loading
+    setPage((prevPage) => prevPage + 1);
   }
 
   return (
@@ -177,7 +64,7 @@ const News = (props) => {
       <InfiniteScroll
         dataLength={articles.length}
         next={fetchMoreData}
-        hasMore={hasMore}
+        hasMore={articles.length < totalResults}
         loader={<Spinner />}
       >
         <div className="container">
